4174377b7da7501490b2a0d5ba180879dab2ae48
yyymmp <35790776+yyymmp@users.noreply.github.com>
2023-02-03 17:16:21 +0800

    props

---
+++ vue3/my-vue-app/src/components/Content.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <!-- 单独功能模块的封装 父组件也可以是作为子组件被引用 -->
+        <Hello ></Hello>
+        <h2>我是组件内容</h2>
+        <h2>{{ msg }}</h2>
+        <!-- 使用组件 -->
+        <button @click="msg = 'hi'">按钮</button>
+        <!-- 父组件向子组件传值 标签是自定义的,可以动态绑定 如果是死数据，也可无需动态绑定-->
+        <Hello :info="msg"  aaa="111"></Hello>
+    </div>
+
+</template>   
+<script>
+// 在组件中引用组件
+import Hello from "./Hello.vue"
+export default{
+    data:function(){
+        // 返回对象,因为data是一个函数,函数拥有局部作用域,当这样返回数据时,该组件被引用时,每一次引用的实例的数据都是互不干扰的
+        return {
+            msg:123
+        }
+    },
+    // 注册组件
+    components:{
+        Hello
+    }
+}
+</script>     
---
+++ vue3/my-vue-app/src/components/Hello.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <h2>hello</h2>
+        <!-- 使用父组件数据 -->
+        <h2>{{ info }}</h2>
+        <h2>{{ aaa }}</h2>
+    </div>
+
+</template>        
+<script>
+export default{
+    // 使用props接受父组件传参 数组形式接受
+    // props: ['info',"aaa"]
+    //使用对象形式接受并设置接受类型,也可以设置默认值  不符合时会报警告
+    props:{
+        // info:String,
+        info:{
+            // 类型
+            type:String,
+            // 设置默认值
+            default:"info默认值",
+            //设置必传 否则抛出警告
+            required: true
+        },
+        aaa:"子组件默认值"
+    }
+
+
+}
+</script>
